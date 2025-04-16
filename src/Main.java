3a843d5a3bd68778846fba75be93a6642e63e7da
Eyal Noy <eyalnoy@gmail.com>
2025-04-16 15:33:35 -0500

    4.16

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        QuestionService service =  new QuestionService();
+        service.displaQuestions();
+
     }
 }
--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,11 +1,19 @@
+public class QuestionService {
 
+    Question[] questions = new Question[5];
 
-public class QuestionService
-{
+    {
+        questions[0] = new Question(1, "What is the size of an int in Java?", "2", "6", "8", "32", "32");
+        questions[1] = new Question(2, "What is the default value of a boolean in Java?", "true", "false", "0", "null", "false");
+        questions[2] = new Question(3, "Which keyword is used to inherit a class in Java?", "implement", "extends", "inherits", "super", "extends");
+        questions[3] = new Question(4, "Which method is used to start a thread in Java?", "run()", "start()", "execute()", "begin()", "start()");
+        questions[4] = new Question(5, "Which of these is not a Java primitive type?", "int", "float", "String", "char", "String");
+    }
 
-    Question[] questions = new Question[5]; 
- 
- 
+    public void displayQuestions()
+    {
+    }
 
-    
 }
+
+
